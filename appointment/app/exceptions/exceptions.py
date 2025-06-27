a5bdf92bf523ed90d6819d6a30e5e34b97553425
pdnlk <facebal900@mail.ru>
2025-06-27 16:43:15 +0300

    feat: добавил новые кастомные ошибки

--- appointment/app/exceptions/exceptions.py
+++ appointment/app/exceptions/exceptions.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
 class AppError(Exception):
@@ -13,14 +13,30 @@ class ConflictException(AppError):
     pass
 
 
+class IncorrectDataException(AppError):
+    pass
+
+
 class AppointmentAlreadyExistsException(ConflictException):
     def __init__(self, doctor_id: int, start_time: datetime):
         self.doctor_id = doctor_id
         self.start_time = start_time
-        super().__init__(f'Appointment already exists for doctor {doctor_id} and start time {start_time}')
+        super().__init__(f'Запись уже существует для doctor_id {doctor_id} и start_time {start_time}')
 
 
 class AppointmentNotFoundException(NotFoundException):
     def __init__(self, appointment_id: int):
         self.appointment_id = appointment_id
-        super().__init__(f'Appointment with id {appointment_id} not found')
+        super().__init__(f'Записи с id {appointment_id} не существует')
+
+
+class AvailableAppointmentsNotFoundException(NotFoundException):
+    def __init__(self, target_date: date):
+        self.target_date = target_date
+        super().__init__(f'Нет доступных записей для выбранной даты {self.target_date}')
+
+
+class IncorrectAppointmentTimeException(IncorrectDataException):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
--- appointment/app/exceptions/exceptions_handlers.py
+++ appointment/app/exceptions/exceptions_handlers.py
@@ -5,7 +5,14 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 from app.core.custom_logger import logger
-from app.exceptions.exceptions import ConflictException, NotFoundException
+from app.exceptions.exceptions import ConflictException, NotFoundException, IncorrectDataException
+
+
+async def incorrect_data_exception_handler(_: Request, exc: IncorrectDataException) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"detail": str(exc)}
+    )
 
 
 async def not_found_exception_handler(_: Request, exc: NotFoundException) -> JSONResponse:
@@ -23,10 +30,11 @@ async def conflict_exception_handler(_: Request, exc: ConflictException) -> JSON
 
 
 async def database_connection_exception_handler(_: Request, exc: ConnectionDoesNotExistError) -> JSONResponse:
+    # TODO можно сделать отправку сообщения об ошибке в бота или email
     logger.exception(exc)
     return JSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content={"detail": "Database connection error"}
+        content={"detail": "Internal Server Error"}
     )
 
 
@@ -34,11 +42,12 @@ async def database_execute_exception_handler(_: Request, exc: SQLAlchemyError) -
     logger.exception(exc)
     return JSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content={"detail": "Database execute query error"}
+        content={"detail": "Internal Server Error"}
     )
 
 
 exception_handlers = {
+    IncorrectDataException: incorrect_data_exception_handler,
     NotFoundException: not_found_exception_handler,
     ConflictException: conflict_exception_handler,
     ConnectionDoesNotExistError: database_connection_exception_handler,
