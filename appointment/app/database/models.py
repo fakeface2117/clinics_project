4d3c061d00c49d1da1b432cb39868b4250085af3
pdnlk <facebal900@mail.ru>
2025-06-27 16:43:40 +0300

    refactor: убрал другие таблицы

--- appointment/app/database/models.py
+++ appointment/app/database/models.py
@@ -1,35 +1,8 @@
-from datetime import datetime, date
-from uuid import UUID, uuid4
+from datetime import datetime
 
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, relationship, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database.base import Base
-
-
-# class PatientsTable(Base):
-#     """Таблица пациентов"""
-#     __tablename__ = 'patients'
-#     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
-#     first_name: Mapped[str] = mapped_column(nullable=False)
-#     last_name: Mapped[str] = mapped_column(nullable=False)
-#     birthday: Mapped[date] = mapped_column(nullable=False)
-#     gender: Mapped[str] = mapped_column(nullable=False)
-#
-#     appointments: Mapped[list['AppointmentTable']] = relationship('AppointmentTable', back_populates='patient')
-#
-#
-# class DoctorsTable(Base):
-#     """Таблица врачей"""
-#     __tablename__ = 'doctors'
-#     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
-#     first_name: Mapped[str] = mapped_column(nullable=False)
-#     last_name: Mapped[str] = mapped_column(nullable=False)
-#     birthday: Mapped[date] = mapped_column(nullable=False)
-#     gender: Mapped[str] = mapped_column(nullable=False)
-#     specialization: Mapped[str] = mapped_column(nullable=False)
-#
-#     appointments: Mapped[list['AppointmentTable']] = relationship('AppointmentTable', back_populates='doctor')
 
 
 class AppointmentTable(Base):
@@ -37,18 +10,8 @@ class AppointmentTable(Base):
     __tablename__ = "appointments"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    # doctor_id: Mapped[UUID] = mapped_column(ForeignKey('doctors.id'), nullable=False)
-    # patient_id: Mapped[UUID] = mapped_column(ForeignKey('patients.id'), nullable=False)
     doctor_id: Mapped[int] = mapped_column(nullable=False)
     patient_id: Mapped[int] = mapped_column(nullable=False)
     start_time: Mapped[datetime] = mapped_column(nullable=False)
     end_time: Mapped[datetime] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=True)
-
-    # Уникальное ограничение для пары doctor_id + start_time
-    # __table_args__ = (
-    #     {'unique_constraint': ('doctor_id', 'start_time')},
-    # )
-
-    # patient: Mapped['PatientsTable'] = relationship(PatientsTable, back_populates='appointments')
-    # doctor: Mapped['DoctorsTable'] = relationship(DoctorsTable, back_populates='appointments')
